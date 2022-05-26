bfc509a8d8708cb50adc1bd161fb9e4f037defc2
herolada <herolada@fel.cvut.cz>
2022-05-26 19:25:32 +0200

    tweeks

--- tester.py
+++ tester.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-fig,ax = plt.subplot()
+fig,ax = plt.subplots()
 x = np.array([0,2,2,0])
 y = np.array([0,0,2,2])
 
-ax.fill(x,y,c='#FF0000', alpha=0.2, zorder = 2, fill=False, hatch = '/')
+plt.rcParams['hatch.linewidth'] = 10  # previous pdf hatch linewidth
+
+ax.fill(x,y,c='#FF0000', alpha=0.8, zorder = 2, fill=False, hatch = '/')
+plt.show()
